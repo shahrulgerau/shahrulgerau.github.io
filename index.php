0d95400817a8461166c1cede83d2abb3efe9e061
DESKTOP-6TCVI20\snzon <shahrulnizam.ismai@gmail.com>
2017-03-13 10:24:45 +0800

    ss

--- index.php
+++ index.php
@@ -7,14 +7,3 @@
 <h3> Gerau in Action </h3>
 <p>I'm hosted with GitHub Pages.</p>
 </body>
-=======
-<!DOCTYPE html>
-<html>
-<body>
-<h1>Hello World</h1>
-<h2>WELCOME</h2>
-<h3> Gerau in Action </h3>
-<p>I'm hosted with GitHub Pages.</p>
-</body>
->>>>>>> 3dc195b06af485f279a6b998650b392cc25e75cd:index.php
-</html>
